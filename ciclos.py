fcdb130d2e55b314ef4cc89e875bbed01214e513
norkaVasquez <95002407+norkaVasquez@users.noreply.github.com>
2021-11-26 03:01:13 +0100

    Add files via upload

---
+++ ciclos.py
@@ -0,0 +1,23 @@
+diccionario={"Mario":3,"Jose":7,"Machad":47,"Cedeño":12}
+diccionario
+ i=0
+ j=0
+ for i,j in diccionario.items():
+     d=j
+     if j==d:
+         d=j
+     elif j<=d:
+         pass
+     elif j>=d:
+         d=J
+print ("El Valor mas Alto es: ", d)
+i=0
+J=0
+for i,j in diccionario.items():
+    if j==d:
+        d=j
+    elif j<=d:
+        d=j
+    elif j>=d:
+    pass
+     print("el valor mas bajo es", d)
